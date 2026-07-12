10c848ecb89adec22608b76510ea17e5fcafea94
Andrew Wallace <japawallace16@gmail.com>
2026-07-12 12:40:59 +0700

    chore: init

---
+++ UpDish/UpDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpDishApp.swift
+//  UpDish
+//
+//  Created by Andrew Wallace on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UpDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpDishTests/UpDishTests.swift
@@ -0,0 +1,18 @@
+//
+//  UpDishTests.swift
+//  UpDishTests
+//
+//  Created by Andrew Wallace on 10/07/26.
+//
+
+import Testing
+
+struct UpDishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
